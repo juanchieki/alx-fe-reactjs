69c568442befe5ff551cd443633c16c4f8b57aed
Paschal <Paschalflexy10@gmail.com>
2025-07-13 17:18:58 +0100

    Add UserProfile component and display user info via props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,17 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
+
+
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
+
 
 export default App;
