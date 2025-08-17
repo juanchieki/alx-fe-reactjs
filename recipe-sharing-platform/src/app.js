a424d37cf480cf40a5f050318dbae24b0ec45037
Juanita <juanchieki@gmail.com>
2025-08-17 10:49:16 +0100

    add HomePage.jsx

---
+++ recipe-sharing-platform/src/app.js
@@ -0,0 +1,19 @@
+// src/App.js
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
