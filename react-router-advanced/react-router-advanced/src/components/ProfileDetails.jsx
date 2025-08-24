359eca853eae9ab691b0eab3e9283b271eb86eab
Juanita <juanchieki@gmail.com>
2025-08-24 19:53:22 +0100

    Remove unwanted directory from repo

--- react-router-advanced/react-router-advanced/src/components/ProfileDetails.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProfileDetails() {
-  return (
-    <div>
-      <h2>Profile Details</h2>
-      <p>This is a nested route within Profile.</p>
-    </div>
-  );
-}
--- react-router-advanced/react-router-advanced/src/components/ProfileSettings.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProfileSettings() {
-  return (
-    <div>
-      <h2>Profile Settings</h2>
-      <p>Update your preferences here (nested route).</p>
-    </div>
-  );
-}
